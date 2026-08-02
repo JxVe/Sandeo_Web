f3efd4da8f729c6b0535392673739dbc0df35069
JxVe <jxxvee7@gmail.com>
2026-08-02 12:12:08 -0500

    Update Alliances.js

--- assets/Alliances.js
+++ assets/Alliances.js
@@ -10,7 +10,11 @@
   grid.innerHTML = ALLIANCES.map((a, i) => `
     <div class="simple-card" data-i="${i}">
       <div class="simple-card-img" style="background-image:url('${a.logo}')"></div>
-      <div class="simple-card-name">${a.name}</div>
+      <div class="simple-card-body">
+        <div class="simple-card-name">${a.name}</div>
+        <div class="simple-card-meta">${a.members}</div>
+        <span class="simple-card-btn">Ver perfil &rarr;</span>
+      </div>
     </div>
   `).join("");
 
